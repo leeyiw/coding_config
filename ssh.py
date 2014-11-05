b02593aaffc5ada1c2fe21944ba49e5a1f73448f
leeyiw <leeyiw@gmail.com>
2014-11-05 11:08:51 +0800

    add first connect fingerprints yes

--- ssh.py
+++ ssh.py
@@ -6,11 +6,14 @@ import time
 
 USERNAME = 'guest'
 PASSWORD = 'guest_12345687'
-HOST = 'leeyiw.org'
+HOST = 'vpn.leeyiw.org'
 
 def main():
     child = pexpect.spawn('ssh -qTfnN -D 0.0.0.0:7070 %s@%s' % (USERNAME, HOST))
-    child.expect('password:')
+    index = child.expect(['\(yes/no\)\? ', 'password: '])
+    if index == 0:
+        child.sendline('yes')
+        child.expect('password:')
     child.sendline(PASSWORD)
     time.sleep(1)
 
