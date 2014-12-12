6fee3fbbc5118c8a180cfd4957dbcb1bbb1f2997
leeyiw <leeyiw@gmail.com>
2014-12-12 14:54:02 +0800

    enable golang plugin

--- set_zsh.sh
+++ set_zsh.sh
@@ -1,7 +1,17 @@
+# Set theme
 ZSH_THEME="steeef"
+
+# Set plugins
+plugins=(git golang)
+
+# Disable history sharing
 unsetopt SHARE_HISTORY
+
+# Suppress dircolors error on Mac OS X
 if command -v dircolors >/dev/null 2>&1
 then
     eval `dircolors ~/.dircolors`
 fi
+
+# Finally run oh-my-zsh.sh
 source $ZSH/oh-my-zsh.sh
