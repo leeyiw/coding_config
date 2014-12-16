1dfbba5eda7eee01157660bfcc4329e26c96aeba
leeyiw <leeyiw@gmail.com>
2014-12-16 19:40:16 +0800

    make disable history sharing right

--- set_zsh.sh
+++ set_zsh.sh
@@ -4,14 +4,14 @@ ZSH_THEME="steeef"
 # Set plugins
 plugins=(git golang)
 
-# Disable history sharing
-unsetopt SHARE_HISTORY
-
 # Suppress dircolors error on Mac OS X
 if command -v dircolors >/dev/null 2>&1
 then
     eval `dircolors ~/.dircolors`
 fi
 
-# Finally run oh-my-zsh.sh
+# Run oh-my-zsh.sh
 source $ZSH/oh-my-zsh.sh
+
+# Disable history sharing
+unsetopt SHARE_HISTORY
