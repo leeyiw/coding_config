830479548bcd19d1b02630775b7bd19c6a720742
leeyiw <leeyiw@gmail.com>
2016-09-09 10:30:51 +0800

    转换dmesg时间的脚本

---
+++ dmesg.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from datetime import datetime, timedelta
+import subprocess
+
+def main():
+    with open('/proc/uptime', 'r') as f:
+        line = f.readline()
+        uptime = float(line.split(' ')[0])
+    now = datetime.now()
+    output = subprocess.check_output(['dmesg'])
+    for line in output.splitlines():
+        old_time_str = line[1:line.find("] ")] 
+        old_ts = float(old_time_str)
+        time_str = str(now - timedelta(seconds=(uptime - old_ts)))
+        new_line = line.replace(old_time_str, time_str, 1)
+        print new_line
+
+
+if __name__ == "__main__":
+    main()
